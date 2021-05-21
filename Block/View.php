997857c851d9e2c4ba3c55903457ca4f1f81d7f9
Amjada2 <amjada@onlinesupporter.nl>
2021-05-21 09:29:19 +0200

    Initial commit

---
+++ Block/View.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Learning\HelloPage\Block;
+
+
+class View extends \Magento\Framework\View\Element\Template
+{
+
+
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context
+
+    )
+    {
+
+        parent::__construct($context);
+    }
+}
